9a7bc1896565aacf0379e77e8addde66ca282d23
Catarina Guimarães <catarina.guimaraes15@gmail.com>
2024-10-29 19:43:42 -0300

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-// import Image from "next/image";
 import { getAddress } from "../../get-address";
 import { NavBar } from "../components/header";
 import { useState } from "react";
@@ -15,56 +14,55 @@ interface Address {
   localidade: string;
 }
 
-const addressList: Address[] = [
-  {
-    id: randomUUID(),
-    logradouro: "Rua das Flores",
-    bairro: "Jardim Primavera",
-    cep: "12345-678",
-    complemento: "Apto 101",
-    estado: "SP",
-    localidade: "São Paulo",
-  },
-  {
-    id: randomUUID(),
-    logradouro: "Avenida Paulista",
-    bairro: "Centro",
-    cep: "87654-321",
-    complemento: "Próximo ao metrô",
-    estado: "SP",
-    localidade: "São Paulo",
-  },
-  {
-    id: randomUUID(),
-    logradouro: "Rua Rio Branco",
-    bairro: "Centro",
-    cep: "11223-445",
-    complemento: "Casa",
-    estado: "RJ",
-    localidade: "Rio de Janeiro",
-  },
-  {
-    id: randomUUID(),
-    logradouro: "Avenida Brasil",
-    bairro: "Jardim América",
-    cep: "33445-667",
-    complemento: "Bloco B",
-    estado: "MG",
-    localidade: "Belo Horizonte",
-  },
-  {
-    id: randomUUID(),
-    logradouro: "Rua das Oliveiras",
-    bairro: "Bela Vista",
-    cep: "55667-889",
-    complemento: "Próximo ao parque",
-    estado: "RS",
-    localidade: "Porto Alegre",
-  },
-];
-
 export default function Home() {
   const [address, setAddress] = useState<Address | null>(null);
+  const [addressList, setAddressList] = useState<Address[]>([
+    {
+      id: randomUUID(),
+      logradouro: "Rua das Flores",
+      bairro: "Jardim Primavera",
+      cep: "12345-678",
+      complemento: "Apto 101",
+      estado: "SP",
+      localidade: "São Paulo",
+    },
+    {
+      id: randomUUID(),
+      logradouro: "Avenida Paulista",
+      bairro: "Centro",
+      cep: "87654-321",
+      complemento: "Próximo ao metrô",
+      estado: "SP",
+      localidade: "São Paulo",
+    },
+    {
+      id: randomUUID(),
+      logradouro: "Rua Rio Branco",
+      bairro: "Centro",
+      cep: "11223-445",
+      complemento: "Casa",
+      estado: "RJ",
+      localidade: "Rio de Janeiro",
+    },
+    {
+      id: randomUUID(),
+      logradouro: "Avenida Brasil",
+      bairro: "Jardim América",
+      cep: "33445-667",
+      complemento: "Bloco B",
+      estado: "MG",
+      localidade: "Belo Horizonte",
+    },
+    {
+      id: randomUUID(),
+      logradouro: "Rua das Oliveiras",
+      bairro: "Bela Vista",
+      cep: "55667-889",
+      complemento: "Próximo ao parque",
+      estado: "RS",
+      localidade: "Porto Alegre",
+    },
+  ]);
   const [loading, setLoading] = useState(false);
   const [textValue, setTextValue] = useState("");
 
@@ -72,30 +70,38 @@ export default function Home() {
     setLoading(true);
     try {
       const result = await getAddress(textValue);
-      setAddress(result);
-      console.log(result);
+
+      const newAddress = {
+        id: randomUUID(),
+        ...result,
+      };
+
+      setAddress(newAddress);
+
+      // Adiciona o novo endereço ao início da lista
+      setAddressList((prevList) => [newAddress, ...prevList]);
     } catch (error) {
       console.error("Ocorreu um erro inesperado:", error);
     } finally {
       setLoading(false);
+      setTextValue("");
     }
   }
 
   return (
     <div className="bg-[#000000] text-white min-h-screen flex flex-col justify-center items-center">
       <NavBar />
-
-      <main className="p-8 bg-[#231B33] rounded-lg max-w-3xl">
+      <main className="p-8 bg-[#231B33] rounded-lg w-[500px] h-[500px] max-w-3xl overflow-auto">
         <h1 className="text-3xl mb-8">Buscador de Endereço</h1>
 
         <div className="flex flex-col gap-2">
           <div>
             <input
               onChange={(e) => setTextValue(e.target.value)}
-              value=""
+              value={textValue}
               type="text"
               id="cep"
-              className="rounded-lg shadow-lg px-4 py-3 w-full "
+              className="text-black rounded-lg shadow-lg px-4 py-3 w-full"
               placeholder="Digite um CEP válido"
             />
           </div>
@@ -103,7 +109,7 @@ export default function Home() {
             onClick={handleGetAddress}
             className={`${
               loading ? "opacity-30" : ""
-            } w-fit px-5 py-3 rounded-lg bg-[#FE0096] transition duration-500 ease-in-out hover:bg-pink-800  text-white my-2`}
+            } w-fit px-5 py-3 rounded-lg bg-[#FE0096] transition duration-500 ease-in-out hover:bg-pink-800 text-white my-2`}
             disabled={loading}
           >
             {loading ? "Carregando..." : "Obter endereço"}
@@ -112,7 +118,7 @@ export default function Home() {
           <span>
             Endereço:{" "}
             {address
-              ? ` ${address.logradouro}, ${address.bairro}`
+              ? `${address.logradouro}, ${address.bairro}`
               : "Nenhum endereço obtido"}
           </span>
         </div>
@@ -120,7 +126,7 @@ export default function Home() {
         <div className="flex flex-col gap-4 mt-6">
           <ul>
             {addressList.map((address) => (
-              <li key={address.id}>{address.logradouro}</li>
+              <li key={address.id}>{address.localidade}</li>
             ))}
           </ul>
         </div>
